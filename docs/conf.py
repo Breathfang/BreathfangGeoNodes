1de26d6e565136cff2c9e8f299bc1615ddcf4183
Breathfang (Drageon DB) <110581007+Breathfang@users.noreply.github.com>
2025-04-22 13:38:26 +0700

    s

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,20 @@
 master_doc = 'index'
+
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    'flyout_display': 'hidden',
+    'version_selector': True,
+    'language_selector': True,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
